3dc399e692717eee06eeb4d8d7df5712465e05e1
Gyungwook Lee (Gyongyon) <126635376+KWLeekr@users.noreply.github.com>
2025-02-11 16:20:28 +0900

    Update chain.py

--- howtomakechains/chain.py
+++ howtomakechains/chain.py
@@ -1,3 +1,10 @@
+from langchain.prompts import ChatPromptTemplate
+
+template = ChatPromptTemplate.from_messages([
+    ("system", "a list generating machine Everything you are asked will be answered with a comma separated list of max {max_items} in lowercase. Do not reply with anything else"),
+    ("human", "{question}"),
+])
+
 chain = template | chat | CommaOutputParser()
 
 chain.invoke({
