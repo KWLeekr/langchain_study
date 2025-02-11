be8018b995f4995dda57293bfced9f56a1b436aa
Gyungwook Lee (Gyongyon) <126635376+KWLeekr@users.noreply.github.com>
2025-02-11 16:47:01 +0900

    Update and rename LLM_block.py to LLM_block_template.py

--- howtomakechains/LLM_block_template.py
+++ howtomakechains/LLM_block_template.py
@@ -4,14 +4,21 @@ from langchain.chat_models import ChatOpenAI
 
 #-------------------------------------------
 
-# 무슨 봇 쓸지 지정
+# <무슨 봇 쓸지, MAX토큰 등 지정 코드>
+
+# 여기서 설정할 수 있는 내용
+# AI모델
+# 맥스토큰
+
 chat = ChatOpenAI(
     model="gpt-4o-mini", 
     max_tokens=150
 )
 
 #-------------------------------------------
-# 이 부분은, 그냥 parser임 (지 마음대로 만드는 것)
+
+# <이 부분은, 그냥 parser임 (지 마음대로 만드는 것)>
+# 이 부분은, 사용자가 커스텀 해서 만든 부분임
 
 from langchain.schema import BaseOutputParser
 
@@ -25,7 +32,7 @@ from langchain.prompts import ChatPromptTemplate
 #-------------------------------------------
 
 # 이부분이 시스템 메시지, 사용자 메시지와 매개변수 집어넣는 부분
-# 이 부분은, dify의 LLM 세팅하는 부분이라고 생각하자.
+# 이 부분은, dify의 LLM 세팅하는 부분이라고 생각하자. -> 나중에 여기 .md파일을 연결할 수 있도록 하자.
 
 template = ChatPromptTemplate.from_messages([
     ("system", "a list generating machine Everything you are asked will be answered with a comma separated list of max {max_items} in lowercase. Do not reply with anything else"),
@@ -35,12 +42,12 @@ template = ChatPromptTemplate.from_messages([
 #-------------------------------------------
 
 # template을 chat에 넣고, 그 결과를 CommaOutputParser에 넣은 것
-# 맨 마지막에 CommaOutputParser를 안 넣으면, 그냥 답변이 돌아옴
+# 맨 마지막에 CommaOutputParser를 안 넣으면, 그냥 답변이 돌아옴 (이 위에 있는 요소들을 순차적으로 처리하기 위한 코드)
 
 chain = template | chat | CommaOutputParser()
 
 #-------------------------------------------
-# 아래는, dify 시작 부분에서 매개변수 Placeholder와 sys.query 집어넣는 코드라고 생각하자. (일단은)
+# 아래는, dify 시작 부분에서 Placeholder(매개변수)와 sys.query 등을 집어넣는 코드라고 생각하자. (일단은)
 
 chain.invoke({
     "max_items":5,
