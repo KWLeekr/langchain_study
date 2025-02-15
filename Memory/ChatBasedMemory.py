fc1b7bbc72da3ca393e728bb764712c9c0a45f69
Gyungwook Lee (Gyongyon) <126635376+KWLeekr@users.noreply.github.com>
2025-02-15 17:26:10 +0900

    Update ChatBasedMemory.py

--- Memory/ChatBasedMemory.py
+++ Memory/ChatBasedMemory.py
@@ -3,16 +3,21 @@ from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.callbacks import StreamingStdOutCallbackHandler
 
 # -----------------------------------------------------------------------
+# 간단한 해설 (완전 정확하지는 않음)
 
 # Langchain은 chain이라는 것을 만들어서, LLM의 워크플로우를 구성한다.
 # 그러기 위해서 해야할 일은 다음과 같다.
 
 # 1. LLM 설정 -> LLM 요소를 만들고, 그안에 어떤 모델을 쓸지, 그 모델의 temperature, 등 옵션을 설정한다.
 # 2. Memory 설정 -> 어떤 방식의 기억(메모리)을 사용할지 설정한다. (LLM으로 요약된 메모리 or 대화내용 그대로 넣기) (저장하는 범위, 용량)
-# 3. Template 설정 -> 현재의 기억을 어떻게 보여줄지 설정 (System prompt, 지금까지의 채팅 내역, 현재 입력한 내용 등을 어떤 형태로 보여줄지 설정 가능) 
-# 4. 지금까지 만든 LLM, Memory, Template를 넣은 "Chain"을 만든다. -> 이게 LLM을 사용하기 위한 모듈?이 되는 것
+# 3. Template 설정 -> 현재의 상황을 어떻게 보여줄지 설정 (System prompt, memory, 현재 입력한 내용 등을 어떤 형태로 보여줄지 설정 가능) 
+# 4. 지금까지 만든 LLM, Memory, Template의 설정값이 들어있는 변수들을 넣어서 "Chain"을 만든다. -> 이렇게 LLM사용을 위한 설정이 끝남.
 
+# chain을 통해 지금까지 설정한 Memory, Template을 LLM에게 전달 할 수 있음 -> LLM을 원하는대로 사용할 수 있음
 # **완성** : 이 Chain을 실행하면, LLM과 대화할 수 있는 구조이다.
+
+
+# 좋은 점은, 같은 Memory를 여러 Chain이 공유할 수도 있고, Template도 공유할 수 있음. (이걸 조합해서 여러가지 시스템에 응용할 수 있음)
 
 # -----------------------------------------------------------------------
 
@@ -57,3 +62,10 @@ chain = LLMChain(
 )
 
 # -----------------------------------------------------------------------
+
+# LLM에게 "Hello, My name is gyungwook" 이라고 보내는 것
+# 실제 의미는 Template의 {question}에 들어갈 부분을 넣어서 LLM을 구동한다는 뜻에 더 가깝다.
+
+chain.predict(question="Hello, My name is gyungwook")
+
+
